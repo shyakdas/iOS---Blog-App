d04a075159a0d728b53be1d5ca71ce6f33be056d
Shyak Das <shayak.das007@gmail.com>
2023-09-27 00:29:04 +0530

    [Firebase]Adding firebase

--- BlogApp/Other/AppDelegate.swift
+++ BlogApp/Other/AppDelegate.swift
@@ -5,6 +5,7 @@
 //  Created by Shyak Das on 25/09/23.
 //
 
+import Firebase
 import UIKit
 
 @main
@@ -13,7 +14,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+        FirebaseApp.configure()
         return true
     }
 
