3ff196bdfee789b4ddc57dfcf7510cd35284d8b7
Shyak Das <shayak.das007@gmail.com>
2023-10-11 22:53:55 +0530

    [UI]SignInScreen design

---
+++ BlogApp/Views/SigInHeaderView.swift
@@ -0,0 +1,46 @@
+//
+//  SigInHeaderView.swift
+//  BlogApp
+//
+//  Created by Shyak Das on 11/10/23.
+//
+
+import UIKit
+
+class SigInHeaderView: UIView {
+    
+    private let imageView : UIImageView = {
+        let imageView = UIImageView(image: UIImage(named: "logo"))
+        imageView.contentMode = .scaleAspectFit
+        imageView.backgroundColor = .systemPink
+        return imageView
+    }()
+    
+    private let label : UILabel = {
+        let label = UILabel()
+        label.textAlignment = .center
+        label.numberOfLines = 0
+        label.font = .systemFont(ofSize: 20, weight : .medium)
+        label.text = "Explire millions of articles"
+        return label
+    }()
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        clipsToBounds = true
+        addSubview(label)
+        addSubview(imageView)
+    }
+    
+    required init?(coder : NSCoder) {
+        fatalError()
+    }
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        
+        let size : CGFloat = width/4
+        imageView.frame = CGRect(x : (width - size)/2, y : 10, width: size, height: size)
+        label.frame = CGRect(x: 20, y: imageView.bottom + 10, width: width - 40, height: height - size - 30)
+    }
+}
